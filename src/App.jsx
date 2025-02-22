4a5bf56e9f819dbb28d211069bd48e1943c111fe
bladeonfire2402 <liemp966@gmail.com>
2025-02-22 13:11:50 +0700

    structure the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
+import AuthScreen_Login from './Screens/Clients/Client-AuthScreen/AuthScreen-Login'
+import Client_Layout from './Screens/Clients/Client-Layout'
 
 
 
@@ -6,7 +8,8 @@ function App() {
   return (
     <div className='app'>
       <Routes>
-
+        <Route path='/*' element={<Client_Layout/>}/>
+        <Route path='/admin' element={<AuthScreen_Login/>}/>
       </Routes>
     </div>
   )
---
+++ src/Screens/Clients/Client-Layout.jsx
@@ -0,0 +1,17 @@
+
+import { Route,Routes  } from 'react-router-dom'
+import AuthScreen_Login from './Client-AuthScreen/AuthScreen-Login'
+import AuthScreen_Register from './Client-AuthScreen/AuthScreen-Register'
+
+const Client_Layout = () => {
+  return (
+    <div className='Client-Layout'>
+        <Routes>
+          <Route path='/auth/login' element={<AuthScreen_Login/>}/>
+          <Route path='register' element={<AuthScreen_Register/>}/>
+        </Routes>
+    </div> 
+    )
+}
+
+export default Client_Layout
